fc0e8ccad632a21936ed39b857b50a05ebfdc553
Kunal Sharma <kunalpandit5200@gmail.com>
2026-09-09 15:31:22 +0530

    chore: update Google Analytics and Tag Manager IDs to the correct configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       {/* Google tag (gtag.js) */}
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-18125109960"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18437736792"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -37,8 +37,7 @@ export default function RootLayout({
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-9YKYJB0P6R');
-          gtag('config', 'AW-18125109960');
+          gtag('config', 'AW-18437736792');
         `}
       </Script>
     </html>
